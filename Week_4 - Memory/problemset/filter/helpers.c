549f1dd9ccc4b6c3353141996acd08cbfa13555d
Fausto Santos <faustosantos.work@gmail.com>
2025-11-18 08:37:23 -0300

    Problem Set Week4 - Memory - Filter + Volume

---
+++ Week_4 - Memory/problemset/filter/helpers.c
@@ -0,0 +1,96 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // avarage of values
+    for (int i = 0; i < height; i++) // para cada coluna
+    {
+        for (int j = 0; j < width; j++) // para cada linha
+        {
+            int average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3; // calcular a média dos valores
+            image[i][j].rgbtBlue = average; // atribuir a média ao valor azul
+            image[i][j].rgbtGreen = average; // atribuir a média ao valor verde
+            image[i][j].rgbtRed = average; // atribuir a média ao valor vermelho
+        }
+    }
+    return; // retornar a função
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++) // para cada coluna
+    {
+        for (int j = 0; j < width; j++) // para cada linha
+        {
+            RGBTRIPLE temp = image[i][j]; // armazenar o valor atual
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp; // Por que o -1?
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            int sumBlue = 0;
+            int sumGreen = 0;
+            int sumRed = 0;
+            for(int x = -1; x<1; x++){
+                for(int y=-1; y<2; y++){
+                    sumBlue += image[i+x][j+y].rgbtBlue;
+                    sumGreen += image[i+x][j+y].rgbtGreen;
+                    sumRed += image[i+x][j+y].rgbtRed;
+                }
+            }
+            image[i][j].rgbtBlue = sumBlue/9; 
+            image[i][j].rgbtGreen = sumGreen/9; 
+            image[i][j].rgbtRed = sumRed/9;
+        }
+    }
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    int Gx[3][3];
+    int Gy[3][3];
+    
+        for(int j = 0; j < 3; j++){
+            Gx[0][j] = -1 + j;
+            Gx[1][j] = -2 + 2*j;
+            Gx[2][j] = -1 + j;
+
+            Gy[j][0] = -1 + j;
+            Gy[j][1] = -2 + 2*j;
+            Gy[j][2] = -1 + j;
+        }
+
+    
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            int Gy_sumBlue = 0;
+            int Gy_sumGreen = 0;
+            int Gy_sumRed = 0;
+            for(int x = -1; x<1; x++){
+                for(int y=-1; y<2; y++){
+                    Gx_sumBlue += (image[i+x][j+y].rgbtBlue) * Gx[i+x][j+y];
+                    Gx_sumGreen += (image[i+x][j+y].rgbtGreen) * Gx[i+x][j+y];
+                    Gx_sumRed += (image[i+x][j+y].rgbtRed) * Gx[i+x][j+y];
+                    Gy_sumBlue += (image[i+x][j+y].rgbtBlue) * Gy[i+x][j+y];
+                    Gy_sumGreen += (image[i+x][j+y].rgbtGreen) * Gy[i+x][j+y];
+                    Gy_sumRed += (image[i+x][j+y].rgbtRed) * Gy[i+x][j+y];
+                }
+            }
+            image[i][j].rgbtBlue = (Gx_sumBlue^2 + Gy_sumBlue^2)^(1/2); 
+            image[i][j].rgbtGreen = (Gx_sumGreen^2 + Gy_sumGreen^2)^(1/2); 
+            image[i][j].rgbtRed = (Gx_sumRed^2 + Gy_sumRed^2)^(1/2);
+        }
+    }
+    return;
+}
--- Week_4 - Memory/problemset/volume.c
+++ Week_4 - Memory/problemset/volume.c
@@ -1,12 +1,68 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Verificar CLA
-// // Verificar FILE == WAV
-// // Verificar Output.wav
+// Finalizado!
 
-// open FILE
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
 
-// Check in FILE is WAV
+int main(int argc, char *argv[])
+{
+    // Verificar CLA
+    // // Verificar FILE == WAV
+    // // Verificar Output.wav
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+    //char *ponto = strrchr(argv[1], '.');
+    //if (strcmp(ponto, "wav") != 0) {
+    //  printf("Usage: ./volume input.wav output.wav factor\n");
+    //  return 1;
+    //} else {
+    //    printf("Usage: ./volume input.wav output.wav factor\n");
+    //    return 1;
+    //}
 
-// Boosting Volume
 
-// Close file
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]); // What atof means?
+
+    // TODO: Copy header from input file to output file
+
+    uint8_t header[HEADER_SIZE]; // Defini para onde vai
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output); // por que não posso jogar direto fwrite(input, header_size, 1, output)
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    // enquanto ter o que ler o while faz a alteração, Pq a diferença entre o size, pq um é 8 outro 16
+    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    {
+        // Update volume of sample
+        buffer *= factor;
+        // Write updated sample to new file
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+    // Close files
+    fclose(input);
+    fclose(output);
+}
